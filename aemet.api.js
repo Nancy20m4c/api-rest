a3980c80f8724e7a6d92584f91d8f3346bc8f75c
Nancy20m4c <101662250+Nancy20m4c@users.noreply.github.com>
2022-04-11 12:06:57 +0200

    cambios class

--- aemet.api.js
+++ aemet.api.js
@@ -2,43 +2,55 @@ class AemetApi {
       constructor (){
             this.apiKey = '/?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYW5jeW1tY0Bob3RtYWlsLmVzIiwianRpIjoiZTEyZGFjM2UtZTY3ZC00YWJkLWE3YjMtYTg0NTUwZjgzM2IwIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2NDkxNDQxMzUsInVzZXJJZCI6ImUxMmRhYzNlLWU2N2QtNGFiZC1hN2IzLWE4NDU1MGY4MzNiMCIsInJvbGUiOiIifQ.Af1VtfYDHZxbHiPxwYxaokIonwH7YLCLhVVzQ7MYckY';
             this.aemetApiUrl = 'https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/46250';
-            this.tabla = document.querySelector('#panelPrincipal .tabla-temperatura tbody');
-            this.cargarTempetura();
+            this.tbody = document.querySelector('#panelPrincipal .tabla-temperatura tbody');
+            this.botonHoy = document.querySelector('#hoy');
+            this.botonManyana = document.querySelector('#manyana');
+            this.botonPasadoManyana = document.querySelector('#pasadoManyana');
+           
+
       }
 
       
-
-      cargarTempetura(){
-      fetch(this.aemetApiUrl + this.apiKey)
-      .then(response => response.json())
-      .then(json => {
-  		console.log()
-  	      fetch(json.datos)
-  			.then( response => response.json())
+      
+      cargarTemperatura(dia) {
+            fetch(this.aemetApiUrl + this.apiKey)
+            .then(response => response.json())
+            .then(json => {
+                  fetch(json.datos)
+                  .then( response => response.json())
 			.then(json => {
-                json[0].prediccion.dia[1].temperatura.forEach(temperatura=> {
-                    let tr = document.createElement('tr');
-                    let tdhora = document.createElement('td');
-                    let tdtemperatura = document.createElement('td');
-                    tdhora.innerText = temperatura.periodo;
+                        this.tbody.innerHTML='';
+                        let listaTemperatura = json[0].prediccion.dia[dia].temperatura;
+                        let listaViento =  json[0].prediccion.dia[0].vientoAndRachaMax;
+                        listaTemperatura.forEach(temperatura=> {
+                              let tr = document.createElement('tr');
+                              let tdhora = document.createElement('td');
+                              let tdtemperatura = document.createElement('td');
+                              let tdviento = document.createElement('td');
+                    tdhora.innerText = temperatura.periodo + ':00';
                     tr.append(tdhora);
-                    tdtemperatura.innerText = temperatura.value;
+                    tdtemperatura.innerText = temperatura.value + 'º';
                     tr.append(tdtemperatura);
-                    this.tabla.append(tr);
-                    console.log(temperatura);
-                   
-                    
-                });
-
-                              
+                    /* tdviento.innerText = temperatura.velocidad+ 'km/h'; */
+                    tr.append(tdviento);
+                    this.tbody.append(tr);
+                        });
+                        
+                        
                   });
-          });
-		
-      }
-
+            });
+            
+      }     
+    
 
 }
 
 
 
 const aemet = new AemetApi();
+
+
+aemet.botonHoy.addEventListener('click', function(){ aemet.cargarTemperatura(0)}); 
+aemet.botonManyana.addEventListener('click', function(){ aemet.cargarTemperatura(1)});
+aemet.botonPasadoManyana.addEventListener('click', function(){ aemet.cargarTemperatura(2)});
+
